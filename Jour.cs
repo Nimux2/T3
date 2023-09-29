4a07ada5379aa69ded0194fff448d4bb9f568164
yglady <yann.glady67@gmail.com>
2023-09-29 15:31:51 +0200

    Plugin base de donnée

--- Jour.cs
+++
@@ -1,7 +0,0 @@
-using Godot;
-using System;
-
-public partial class Jour : GodotObject
-{
-	var listePerso = new Godot.Collections.Array{Null,Null,Null,Null};
-}
--- new_script.gd
+++
@@ -1,3 +0,0 @@
-class jour extends Object :
-
-
