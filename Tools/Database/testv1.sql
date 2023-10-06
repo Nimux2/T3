892492cadf21ca7c0b3f95b32fe0270d3fc56e91
yglady <yann.glady67@gmail.com>
2023-10-06 15:19:19 +0200

    Insertions de Traits, Patients et Maladies

--- Tools/Database/testv1.sql
+++ Tools/Database/testv1.sql
@@ -17,14 +17,10 @@ CREATE TABLE Patients (
                           nom text,
                           stress integer,
                           diagno integer,
-                          maladie integer,
                           images integer,
                           CONSTRAINT fk_PatientsImage
                               FOREIGN KEY (images)
-                                  REFERENCES Image_Patient(id),
-                          CONSTRAINT fk_PatientsMaladie
-                              FOREIGN KEY (maladie)
-                                  REFERENCES Maladies(id)
+                                  REFERENCES Image_Patient(id)
 );
 
 CREATE TABLE Reponses (
@@ -105,3 +101,54 @@ CREATE TABLE Image_Patient (
                                img_peur text,
                                img_colere text
 );
+
+INSERT INTO Maladies (id, nom) VALUES
+  (1, 'Cancer'),
+  (2, 'Diabète'),
+  (3, 'Hypertension'),
+  (4, 'Asthme'),
+  (5, 'Arthrite'),
+  (6, "Maladie d'Alzheimer"),
+  (7, 'Maladie de Parkinson'),
+  (8, 'Sclérose en plaques'),
+  (9, 'VIH/SIDA'),
+  (10, 'Maladie de Lyme'),
+  (11, 'Anglais'),
+  (12, 'Maladie cardiovasculaire'),
+  (13, 'Obésité'),
+  (14, 'Dépression'),
+  (15, 'Anxiété');
+
+INSERT INTO Patients (nom,stress,diagno) VALUES
+	('Françis',0,0),
+	('Bob',0,0),
+	('Michel',0,0),
+	('Saïf',0,0),
+	('Yann',0,0),
+	('Thomas',0,0),
+	('Daniel',0,0),
+	('Jean-Michel-François-d''Esperey',0,0),
+	('Mamadou',0,0),
+	('Mohammed',0,0),
+	('Bernard',0,0),
+	('Tituan',0,0),
+	('Jean Lassale',0,0),
+	('Manu',0,0),
+	('Big Joe',0,0),
+	('Zimmy',0,0),
+	('Kim',0,0),
+	('Eric',0,0),
+	('Marine',0,0),
+	('Jean-Mâ',0,0),
+	('Angela',0,0),
+	('Chirac',0,0),
+	('Sarko',0,0),
+	('Donald',0,0);
+
+INSERT INTO Traits(nom,stress_effect,diagno_effect) VALUES
+	('Stupide',25,25),
+	('Raciste',50,40),
+	('Paranoïaque',90,80),
+	('Gentil',30,45),
+	('Jovial',25,30),
+	('Intelligent',60,90);
